b319b63eaee2e1f3019690f968fa4c777ef666ef
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2024-01-28 15:54:51 +0700

    app(Navbar): navbar progress v1

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { PRODUCT_CATEGORIES } from "@/config";
+import { NavItem } from "@/components";
+import { useState } from "react";
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+  const isAnyOpen = activeIndex !== null;
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+
+        const isOpen = i === activeIndex;
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          ></NavItem>
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavItems;
---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Navbar from "./Navbar";
+import NavItems from "./NavItems";
+import NavItem from "./NavItem";
+
+export { MaxWidthWrapper, Navbar, NavItems, NavItem };
