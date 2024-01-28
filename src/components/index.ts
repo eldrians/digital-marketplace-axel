a9f51b905857f49237ba3ae7c4e2f23f7133dd3f
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2024-01-28 17:54:44 +0700

    app(navbar): completed navbar

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import Navbar from "./Navbar";
 import NavItems from "./NavItems";
 import NavItem from "./NavItem";
+import Cart from "./Cart";
 
-export { MaxWidthWrapper, Navbar, NavItems, NavItem };
+export { MaxWidthWrapper, Navbar, NavItems, NavItem, Cart };
